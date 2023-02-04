b8d4a464e331bf8bb4c5fa0c2210b539bdd38ccb
Andrey Makoldin <avlaaak@gmail.com>
2023-02-04 22:20:43 +0300

    Добавлена функция для загрузки границ регионов

--- common/Validator.class.php
+++ common/Validator.class.php
@@ -6,20 +6,22 @@ mb_internal_encoding('utf-8');
 
 class Validator extends OsmFunctions
 {
-	static    $urls    = [];
-	protected $domain  = '';
-	protected $fields  = [];
-	protected $region  = '';
+	static $urls = [];
+	protected $domain = '';
+	protected $fields = [];
+	protected $region = '';
 	protected $objects = [];
-	protected $filter  = [];
-	public    $useCacheHtml = false; // страницы только из кеша
-	public    $updateHtml   = false; // перезакачать html страницы
+	protected $filter = [];
+	public $useCacheHtml = false; // страницы только из кеша
+	public $updateHtml = false; // перезакачать html страницы
 
 	/* Конструктор - проверка возможности работы с заданным регионом */
 	public function __construct($region)
 	{
-		if (is_array(static::$urls) &&
-			!isset(static::$urls[$region]))
+		if (
+			is_array(static::$urls) &&
+			!isset(static::$urls[$region])
+		)
 			throw new Exception('Unknow region!');
 
 		$this->region = $region;
@@ -70,11 +72,11 @@ class Validator extends OsmFunctions
 
 		$ch = curl_init($url);
 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   // возвращает веб-страницу
-		curl_setopt($ch, CURLOPT_HEADER, false);          // не возвращает заголовки
-		curl_setopt($ch, CURLOPT_USERAGENT, $useragent);  // useragent
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);     // таймаут соединения
-		curl_setopt($ch, CURLOPT_TIMEOUT, 300);           // таймаут ответа
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // возвращает веб-страницу
+		curl_setopt($ch, CURLOPT_HEADER, false); // не возвращает заголовки
+		curl_setopt($ch, CURLOPT_USERAGENT, $useragent); // useragent
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); // таймаут соединения
+		curl_setopt($ch, CURLOPT_TIMEOUT, 300); // таймаут ответа
 
 		//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		//curl_setopt($ch, CURLOPT_MAXREDIRS, 5);
@@ -83,7 +85,9 @@ class Validator extends OsmFunctions
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
 		if (isset($query)) {
-			curl_setopt($ch, CURLOPT_POST, true);         // POST запрос
+			if ($query[0] == '{')
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+			curl_setopt($ch, CURLOPT_POST, true); // POST запрос
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $query); // содержимое POST запроса
 		}
 
@@ -135,7 +139,7 @@ class Validator extends OsmFunctions
 			// склеиваем в строку учитывая одинаковое время работы
 			$res = '';
 			foreach ($a as $time => $days) {
-				$res .= ($res?'; ':'').implode(',', $days).' '.$time;
+				$res .= ($res ? '; ' : '').implode(',', $days).' '.$time;
 			}
 
 			// перечисляем нерабочие дни
@@ -143,63 +147,64 @@ class Validator extends OsmFunctions
 
 			// склеиваем соседние дни
 			for ($i = 1; $i < count($edays); $i++)
-				$res = str_replace($edays[$i-1].','.$edays[$i], $edays[$i-1].'-'.$edays[$i], $res);
-			$res = preg_replace('/-[-\w]+-/', '-',    $res);
-			$res = str_replace('Mo-Su ',      '',     $res);
+				$res = str_replace($edays[$i - 1].','.$edays[$i], $edays[$i - 1].'-'.$edays[$i], $res);
+			$res = preg_replace('/-[-\w]+-/', '-', $res);
+			$res = str_replace('Mo-Su ', '', $res);
 			$res = str_replace('00:00-24:00', '24/7', $res);
 		} else {
 			// Приведение к нижнему регистру
 			$st = ' '.strip_tags(mb_strtolower($st, 'utf-8')).' ';
 
 			// Круглосуточная работа
-			if (mb_stripos($st, 'круглос')) $st = '24/7';
+			if (mb_stripos($st, 'круглос'))
+				$st = '24/7';
 
 			$replace = [
-				'mo'            => 'Mo',
-				'tu'            => 'Tu',
-				'we'            => 'We',
-				'th'            => 'Th',
-				'fr'            => 'Fr',
-				'sa'            => 'Sa',
-				'su'            => 'Su',
+				'mo' => 'Mo',
+				'tu' => 'Tu',
+				'we' => 'We',
+				'th' => 'Th',
+				'fr' => 'Fr',
+				'sa' => 'Sa',
+				'su' => 'Su',
 
-				'пн'            => 'Mo',
-				'вт'            => 'Tu',
-				'ср'            => 'We',
-				'чт'            => 'Th',
-				'пт'            => 'Fr',
-				'сб'            => 'Sa',
-				'вс'            => 'Su',
-				
-				'вск'           => 'Su',
-				
-				'понедельник'   => 'Mo',
-				'вторник'       => 'Tu',
-				'среда'         => 'We',
-				'четверг'       => 'Th',
-				'пятница'       => 'Fr',
-				'суббота'       => 'Sa',
-				'воскресенье'   => 'Su',
+				'пн' => 'Mo',
+				'вт' => 'Tu',
+				'ср' => 'We',
+				'чт' => 'Th',
+				'пт' => 'Fr',
+				'сб' => 'Sa',
+				'вс' => 'Su',
 
-				'выходной'      => 'off',
-				'будни'         => 'Mo-Fr',
-				'выходные'      => 'Sa-Su',
-				'ежедневно'     => 'Mo-Su',
+				'вск' => 'Su',
+
+				'понедельник' => 'Mo',
+				'вторник' => 'Tu',
+				'среда' => 'We',
+				'четверг' => 'Th',
+				'пятница' => 'Fr',
+				'суббота' => 'Sa',
+				'воскресенье' => 'Su',
+
+				'выходной' => 'off',
+				'будни' => 'Mo-Fr',
+				'выходные' => 'Sa-Su',
+				'ежедневно' => 'Mo-Su',
 				'круглосуточно' => 'Mo-Su',
-				' '             => ' ',
-				'c'             => 'с',
-				' и '           => ', ',
-				' в '           => ' ',
-				' до '          => '-',
-				' по '          => '-',
-				'.'             => '',
-				'&ndash;'       => '-',
-				'&mdash;'       => '-',
-				'&nbsp;'        => ' ',
-				'–'             => '-',
-				'—'             => '-',
-				'00:00'         => '24:00',
-				'день'          => ''
+				' ' => ' ',
+				'c' => 'с',
+				' и ' => ', ',
+				' в ' => ' ',
+				' до ' => '-',
+				' по ' => '-',
+				'.' => '',
+				'&ndash;' => '-',
+				'&mdash;' => '-',
+				'&nbsp;' => ' ',
+				'–' => '-',
+				'—' => '-',
+				'00:00' => '24:00',
+				'день' => ''
 			];
 
 			//$st = str_replace(array_keys($replace), $replace, $st);
@@ -212,14 +217,48 @@ class Validator extends OsmFunctions
 			);
 
 			$st = str_replace(
-				['выходной', 'будни', 'выходные', 'ежедневно', 'круглосуточно', ' ', 'c', ' и ', ' в ',
-					' до ', ' по ',
+				[
+					'выходной',
+					'будни',
+					'выходные',
+					'ежедневно',
+					'круглосуточно',
+					' ',
+					'c',
+					' и ',
+					' в ',
+					' до ',
+					' по ',
 					'.',
-					'&ndash;', '&mdash;', '&nbsp;', '–', '—', '00:00', 'день'],
-				['off',      'Mo-Fr', 'Sa-Su',    'Mo-Su',     'Mo-Su',         ' ', 'с', ', ',  ' ',
-					'-',    '-',
+					'&ndash;',
+					'&mdash;',
+					'&nbsp;',
+					'–',
+					'—',
+					'00:00',
+					'день'
+				],
+				[
+					'off',
+					'Mo-Fr',
+					'Sa-Su',
+					'Mo-Su',
+					'Mo-Su',
+					' ',
+					'с',
+					', ',
+					' ',
+					'-',
+					'-',
 					'',
-					'-', '-', ' ', '-', '-', '24:00', ''],
+					'-',
+					'-',
+					' ',
+					'-',
+					'-',
+					'24:00',
+					''
+				],
 				$st
 			);
 
@@ -234,44 +273,95 @@ class Validator extends OsmFunctions
 			$st = preg_replace('#(\D)(\d{1,2})\s*-\s*(\d{1,2})\s#', '$1$2:00-$3:00', $st);
 			$st = preg_replace(
 				[
-					'/понедельник\.?/iu','/вторник\.?/iu','/среда\.?/iu','/четверг\.?/iu','/пятница\.?/iu','/суббота\.?/iu','/воскресенье?\.?/iu',
-					'/пн\.?/iu','/вт\.?/iu','/ср\.?/iu','/чт\.?/iu','/пт\.?/iu','/сб\.?/iu','/вск?\.?/iu',
-					'/\s(\d{1,2})\s*-/','/-\s*(\d{1,2})\s/','/-\s*(\d{1,2});/',
+					'/понедельник\.?/iu',
+					'/вторник\.?/iu',
+					'/среда\.?/iu',
+					'/четверг\.?/iu',
+					'/пятница\.?/iu',
+					'/суббота\.?/iu',
+					'/воскресенье?\.?/iu',
+					'/пн\.?/iu',
+					'/вт\.?/iu',
+					'/ср\.?/iu',
+					'/чт\.?/iu',
+					'/пт\.?/iu',
+					'/сб\.?/iu',
+					'/вск?\.?/iu',
+					'/\s(\d{1,2})\s*-/',
+					'/-\s*(\d{1,2})\s/',
+					'/-\s*(\d{1,2});/',
 					'/[ \s]*—[ \s]*/u',
-					'/([a-z])(\d)/','/\s+/', '/(\d)\s*([A-Z])/', '/([a-z])[^\da-z]+(\d)/', '/ [дп]о /u', '/(^|\D)(\d:)/',
-					'/[  ]?-[  ]?/', '/[^\d\s]00/', '/\s*;/', '/;(\S)/', '/;[; ]+/', '/;\s*$/',
+					'/([a-z])(\d)/',
+					'/\s+/',
+					'/(\d)\s*([A-Z])/',
+					'/([a-z])[^\da-z]+(\d)/',
+					'/ [дп]о /u',
+					'/(^|\D)(\d:)/',
+					'/[  ]?-[  ]?/',
+					'/[^\d\s]00/',
+					'/\s*;/',
+					'/;(\S)/',
+					'/;[; ]+/',
+					'/;\s*$/',
 					//'/([a-z]); ([A-Z])/',  //'/([a-z]); ([A-Z])/i',
 					'/(Mo|Tu|We|Th|Fr|Sa|Su); (Mo|Tu|We|Th|Fr|Sa|Su)/i',
-					'/(\d{2})(\d{2})/','/(\d{1})(\d{2})/', '/-off/',
+					'/(\d{2})(\d{2})/',
+					'/(\d{1})(\d{2})/',
+					'/-off/',
 
 				],
 				[
-					'Mo','Tu','We','Th','Fr','Sa','Su',
-					'Mo','Tu','We','Th','Fr','Sa','Su',
-					' $1:00-', '-$1:00 ', '-$1:00;',
+					'Mo',
+					'Tu',
+					'We',
+					'Th',
+					'Fr',
+					'Sa',
+					'Su',
+					'Mo',
+					'Tu',
+					'We',
+					'Th',
+					'Fr',
+					'Sa',
+					'Su',
+					' $1:00-',
+					'-$1:00 ',
+					'-$1:00;',
 					'-',
 					'$1 $2',
-					' ', '$1; $2', '$1 $2', '-',
-					'${1}0$2', '-', ':00', ';', '; $1', '; ', '',
+					' ',
+					'$1; $2',
+					'$1 $2',
+					'-',
+					'${1}0$2',
+					'-',
+					':00',
+					';',
+					'; $1',
+					'; ',
+					'',
 					'$1-$2',
-					'$1:$2','0$1:$2', ' off',
+					'$1:$2',
+					'0$1:$2',
+					' off',
 
 				],
 				$st
 			);
-			
+
 			$st = preg_replace('/(?<=[0-9]|[A-ZА-Я]),(?=\ *[A-ZА-Я])/ui', ';', $st);
 
 			// Tu-We; Th-Fr-Sa 10:00-11:00 -> Tu-We-Th-Fr-Sa 10:00-11:00
-			$st = preg_replace('/(Mo|Tu|We|Th|Fr|Sa|Su); (Mo|Tu|We|Th|Fr|Sa|Su)/i','$1-$2', $st);
+			$st = preg_replace('/(Mo|Tu|We|Th|Fr|Sa|Su); (Mo|Tu|We|Th|Fr|Sa|Su)/i', '$1-$2', $st);
 
 			$st = str_replace(
 				['-Tu-', '-We-', '-Th-', '-Fr-', '-Sa-'],
-				['-',    '-',    '-',    '-',    '-'],
+				['-', '-', '-', '-', '-'],
 				$st
 			);
-			$st = str_replace('с ',          '',     $st);
-			$st = str_replace('Mo-Su',       '',     $st);
+			$st = str_replace('с ', '', $st);
+			$st = str_replace('Mo-Su', '', $st);
 			$st = str_replace('00:00-24:00', '24/7', $st);
 
 			$st = trim($st);
@@ -279,7 +369,8 @@ class Validator extends OsmFunctions
 			// валидация запрещенных символов
 			//не вкурил, отчего запятая не катит...
 			//if ($st != '24/7' && preg_match('/[^\d:-a-z -]/i', $st)) $st = '';
-			if ($st != '24/7' && preg_match('/[^\d:-a-z -,]/i', $st)) $st = '';
+			if ($st != '24/7' && preg_match('/[^\d:-a-z -,]/i', $st))
+				$st = '';
 
 			$res = $st;
 		}
@@ -290,14 +381,16 @@ class Validator extends OsmFunctions
 	/* Универсальная функция преобразования телефона в стандартный формат */
 	protected function phone($st)
 	{
-		$st = preg_split('/[,;\/]/', $st); $st = $st[0]; // возможно несколько телефонов, берем первый
+		$st = preg_split('/[,;\/]/', $st);
+		$st = $st[0]; // возможно несколько телефонов, берем первый
 		$st = preg_replace('/[^\d()]/', '', $st); // оставляяем цифры и скобки
-		$st = preg_replace('/^7/', '8',     $st); // заменяем первую 7 на 8 (+7 которая)
+		$st = preg_replace('/^7/', '8', $st); // заменяем первую 7 на 8 (+7 которая)
 		$st = preg_replace('/^8?\((.+?)\)(.+)/', '+7-$1-$2', $st, -1, $n); // приводим к стандарту RFC
 		$st = preg_replace('/\(.+/', '', $st); // удаляем оставшиеся скобки, от второго телефона
-		$st = preg_replace('/^8?(\d{3})(\d+)/',   '+7-$1-$2', $st); // выделяем код города - первые 3 цифры
+		$st = preg_replace('/^8?(\d{3})(\d+)/', '+7-$1-$2', $st); // выделяем код города - первые 3 цифры
 		$len = strlen($st);
-		if ($len <= 12 || $len > 14) $st = ''; // что-то пошло не так: получился короткий номер +7-000-12345
+		if ($len <= 12 || $len > 14)
+			$st = ''; // что-то пошло не так: получился короткий номер +7-000-12345
 		$st = str_replace('-', ' ', $st); // формат E.123, DIN 5008
 		return $st;
 	}
@@ -308,7 +401,7 @@ class Validator extends OsmFunctions
 		$res = '';
 		$list = explode($delimiter, $st);
 		foreach ($list as $item)
-			$res .= ($res?'; ':'').$this->phone($item);
+			$res .= ($res ? '; ' : '').$this->phone($item);
 		return $res;
 	}
 
@@ -330,8 +423,10 @@ class Validator extends OsmFunctions
 			if (isset($fields[$k]) && $fields[$k] !== '') {
 				$obj[$k] = ''.$fields[$k];
 			} else {
-				if (is_array($v)) $v = @$v[$this->region];
-				if ($v) $obj[$k] = ''.$v;
+				if (is_array($v))
+					$v = @$v[$this->region];
+				if ($v)
+					$obj[$k] = ''.$v;
 			}
 		return $obj;
 	}
@@ -339,9 +434,9 @@ class Validator extends OsmFunctions
 	/* Фильтрация объекта не нашего региона */
 	protected function isInRegion($city, $region, $text)
 	{
-		$t1 = $this->region == $region;      // совпадение по региону
+		$t1 = $this->region == $region; // совпадение по региону
 		$t2 = preg_match("/$city/u", $text); // совпадение по адресу
-		return ($t1 && $t2) || (!$t1 && !$t2)? 1 : 0; // оба совпали или оба не совпали
+		return ($t1 && $t2) || (!$t1 && !$t2) ? 1 : 0; // оба совпали или оба не совпали
 	}
 
 	/* Принадлежность объекта региону */
@@ -368,6 +463,49 @@ class Validator extends OsmFunctions
 		return $result;
 	}
 
+	// Определение границ региона по точкам
+	protected function GetRegionBorders()
+	{
+		if ($this->region == 'RU') {
+			return true;
+		}
+
+		static $polygons = [[]];
+
+		if (!isset($polygons[0][0]['lat'])) {
+			$polygons = $this->get_geometry($this->region);
+		}
+
+		$maxLat = -90;
+		$minLat = 90;
+		$maxLon = -180;
+		$minLon = 180;
+
+		foreach ($polygons as $polygon) {
+			foreach ($polygon as $point) {
+				if ($point['lat'] > $maxLat) {
+					$maxLat = $point['lat'];
+				}
+				if ($point['lat'] < $minLat) {
+					$minLat = $point['lat'];
+				}
+				if ($point['lon'] > $maxLon) {
+					$maxLon = $point['lon'];
+				}
+				if ($point['lon'] < $minLon) {
+					$minLon = $point['lon'];
+				}
+			}
+		}
+
+		return [
+			'maxLat' => $maxLat,
+			'minLat' => $minLat,
+			'maxLon' => $maxLon,
+			'minLon' => $minLon
+		];
+	}
+
 	/* Добавление объекта во время парсинга страницы */
 	protected function addObject($object)
 	{
@@ -381,7 +519,10 @@ class Validator extends OsmFunctions
 		echo $line;
 		//Кидаем в файлик...
 		if ($_SERVER["DOCUMENT_ROOT"] != "") {
-			$file = $_SERVER["DOCUMENT_ROOT"] . "/data/log.txt";
+			if (!file_exists('/data')) {
+				mkdir('/data', 0777, true);
+			}
+			$file = $_SERVER["DOCUMENT_ROOT"]."/data/log.txt";
 			file_put_contents($file, $line, FILE_APPEND | LOCK_EX);
 		}
 	}
